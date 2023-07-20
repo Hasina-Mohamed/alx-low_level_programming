1d6b92cf44685bd9cb9dbc36a4ec74d181fd1b5d
Hasina-Mohamed <hasuunamohamed2022@gmail.com>
2023-07-20 16:36:21 -0700

    hii

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,17 +6,15 @@
  */
 void positive_or_negative(int i)
 {       
-if (i < 0)
-{       
-printf("%d is %s\n", i, "negative");
-}
-else if (i > 0)
-{       
-printf("%d is %s\n", i, "positive");
-}
-else
-{
-printf("%d is %s\n", i, "zero");
-}
-return;
+	if (i < 0)
+		printf("%d is negative\n", i);
+
+	else if (i > 0)
+   
+		printf("%d is positives\n", i);
+
+	else
+
+		printf("%d is zero\n", i);
+
 }
